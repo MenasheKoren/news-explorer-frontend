1929942574de36f9ce9c2969801b801f893b9647
menashekoren <menashekoren@gmail.com>
2022-03-13 14:53:43 +0200

    Hotfix: Update mobile dropdown styles

--- src/components/HeaderTitle/HeaderTitle.js
+++ src/components/HeaderTitle/HeaderTitle.js
@@ -2,7 +2,11 @@ export function HeaderTitle(props) {
   return (
     <h2
       className="header__title"
-      style={props.dropdownMenuOpen && props.mobile ? { color: "white" } : {}}
+      style={
+        props.dropdownMenuOpen && props.mobile
+          ? { color: "white", position: "fixed" }
+          : {}
+      }
     >
       NewsExplorer
     </h2>
